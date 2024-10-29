279b7dc94e8e62979292e58cf23f022fdf4b4a7e
David Ochsner <davidochsner93@gmail.com>
2024-10-29 10:58:38 +0100

    LERW in 2d (basically untested)

--- include/lerw.hpp
+++ include/lerw.hpp
@@ -1,11 +1,14 @@
+#include <algorithm>
 #include <array>
 #include <cmath>
-#include <format>
 #include <cstdint>
+#include <format>
 #include <functional>
 #include <numeric>
+#include <optional>
 #include <random>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
 namespace lerw {
@@ -15,6 +18,7 @@ using field_t = int;
 struct square {
   template <class T> constexpr auto operator()(T t) const -> T { return t * t; }
 };
+
 struct abs {
   template <class T> constexpr auto operator()(T t) const -> T {
     return std::abs(t);
@@ -49,11 +53,25 @@ template <size_t Dimension> struct Vec {
       res.values[i] = 0;
     return res;
   }
+
+  struct Hash {
+    constexpr std::size_t operator()(const Vec<Dimension> &vec) const {
+      std::size_t hashValue = 0;
+      for (const auto &elem : vec.values) {
+        hashValue ^= std::hash<unsigned int>()(elem) + 0x9e3779b9 +
+                     (hashValue << 6) + (hashValue >> 2);
+      }
+      return hashValue;
+    }
+  };
+
+  bool operator==(const Vec<Dimension> &) const = default;
 };
 
 using Walk = std::vector<Vec<2>>;
 
 struct Stepper {
+  // TODO: These should go into Vec
   static constexpr std::array<Vec<2>, 4> directions{
       {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}};
   std::uniform_int_distribution<uint8_t> distribution{0, directions.size() - 1};
@@ -65,6 +83,8 @@ struct Stepper {
     return p + directions[distribution(gen)];
   }
 };
+
+template <class Step> struct LoopErasingStepper {};
 
 struct LengthStopper {
   size_t length;
@@ -83,17 +103,19 @@ struct L1DistanceStopper {
 };
 
 struct L2DistanceStopper {
-  double distance;
+  double distance_sq;
+
+  L2DistanceStopper(double distance) : distance_sq{distance * distance} {}
 
   constexpr auto operator()(const Walk &walk) const -> bool {
-    return walk.back().L2Sq() > distance;
+    return walk.back().L2Sq() > distance_sq;
   }
 };
 
 template <class Step>
 auto generate_fixed_length_random_walk(std::size_t length, Step stepper)
     -> std::vector<Vec<2>> {
-  return generate_stopped_random_walk(LengthStopper{length}, stepper);
+  return generate_stopped_random_walk(LengthStopper{length}, stepper, length);
 }
 
 template <class Step>
@@ -103,19 +125,72 @@ auto generate_fixed_distance_L1_random_walk(double distance, Step stepper)
 }
 
 template <class Step>
-auto generate_fixed_distance_L2Sq_random_walk(double distanceSquared, Step stepper)
+auto generate_fixed_distance_L2Sq_random_walk(double distanceSquared,
+                                              Step stepper)
     -> std::vector<Vec<2>> {
-  return generate_stopped_random_walk(L2DistanceStopper{distanceSquared}, stepper);
+  return generate_stopped_random_walk(L2DistanceStopper{distanceSquared},
+                                      stepper);
 }
 
-template <class Step, class Stop>
-auto generate_stopped_random_walk(Stop stopper, Step stepper) -> std::vector<Vec<2>> {
-  Walk walk{Vec<2>::Zero()};
-  // TODO: Could take hint about expected length
+template <class Step, class Stop> struct RandomWalkGenerator {
+  Stop stopper;
+  Step stepper;
+  std::optional<size_t> expected_size = std::nullopt;
 
-  while (not stopper(walk))
-    walk.emplace_back(stepper(walk.back()));
-  return walk;
+  constexpr auto operator()() -> Walk {
+    Walk walk{Vec<2>::Zero()};
+
+    if (expected_size)
+      walk.reserve(*expected_size);
+
+    while (not stopper(walk))
+      walk.emplace_back(stepper(walk.back()));
+
+    return walk;
+  }
+};
+
+template <class Step, class Stop> struct LoopErasedRandomWalkGenerator {
+  Stop stopper;
+  Step stepper;
+  std::optional<size_t> expected_size = std::nullopt;
+
+  constexpr auto operator()() -> Walk {
+    std::unordered_set<Vec<2>, Vec<2>::Hash> visited{Vec<2>::Zero()};
+    Walk walk{Vec<2>::Zero()};
+
+    if (expected_size) {
+      // Measure: theres also rehash, and bucket_size
+      visited.reserve(*expected_size);
+      walk.reserve(*expected_size);
+    }
+
+    while (not stopper(walk)) {
+      auto proposed = stepper(walk.back());
+      auto [_, inserted] = visited.insert(proposed);
+
+      // Measure: [[likely]]
+      if (inserted) [[likely]] {
+        walk.emplace_back(std::move(proposed));
+        continue;
+      }
+
+      while (walk.back() != proposed) {
+        visited.erase(walk.back());
+        walk.pop_back();
+      }
+    }
+
+    return walk;
+  }
+};
+
+template <class Generator>
+auto compute_average_length(Generator &&generator, size_t N) -> double {
+  std::vector<size_t> lengths(N);
+  std::ranges::generate(lengths,
+                        [&generator] mutable { return generator().size(); });
+  return 1.0 * *std::ranges::fold_left_first(lengths, std::plus{}) / N;
 }
 
 constexpr auto format_walk(const Walk &walk) -> std::string {
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <print>
+#include <cmath>
 
 #include "lerw.hpp"
 
 auto main() -> int {
   lerw::Stepper stepper{42};
-  std::println("{}", lerw::format_walk(
-                         lerw::generate_fixed_length_random_walk(10, stepper)));
-  std::println("{}", lerw::format_walk(
-                         lerw::generate_fixed_distance_L1_random_walk(10, stepper)));
-  std::println("{}", lerw::format_walk(
-				       lerw::generate_fixed_distance_L2Sq_random_walk(10, stepper)));
+
+  // samples for average
+  const auto& N = 1000;
+
+  auto make_generator = [&stepper](double distance) {
+    return lerw::LoopErasedRandomWalkGenerator{
+        lerw::L2DistanceStopper{distance}, stepper};
+  };
+
+  for (size_t i = 3; i < 9; ++i) {
+    const auto &d = std::pow(2, i);
+    std::println("{}, {}", d, lerw::compute_average_length( make_generator(d), N));
+  }
 }
