23aab2f042ee134c07a2a8e1212c405c9a8c2caa
David Ochsner <davidochsner93@gmail.com>
2024-11-22 10:51:47 +0100

    Implement 1D Point

--- include/point.hpp
+++ include/point.hpp
@@ -7,6 +7,32 @@
 
 namespace lerw {
 
+struct Point1D {
+  int x;
+
+  constexpr auto operator+=(const Point1D &rhs) -> Point1D & {
+    x += rhs.x;
+    return *this;
+  }
+
+  constexpr friend auto operator+(Point1D lhs, const Point1D &rhs) -> Point1D {
+    lhs += rhs;
+    return lhs;
+  }
+
+  constexpr auto operator==(const Point1D &) const -> bool = default;
+
+  consteval static auto Zero() -> Point1D { return {0}; }
+
+  consteval static auto Directions() -> std::array<Point1D, 2> {
+    return {Point1D{1}, {-1}};
+  }
+};
+
+constexpr auto l2sq(const Point1D &p) -> double { return p.x * p.x; }
+
+constexpr auto l1(const Point1D &p) -> double { return abs{}(p.x); }
+
 struct Point3D {
   int x;
   int y;
@@ -46,6 +72,13 @@ constexpr auto l1(const Point3D &p) -> double {
 } // namespace lerw
 
 namespace std {
+
+template <> struct hash<lerw::Point1D> {
+  constexpr auto operator()(const lerw::Point1D &p) const -> std::size_t {
+    return std::hash<int>()(p.x);
+  }
+};
+
 template <> struct hash<lerw::Point3D> {
   constexpr auto operator()(const lerw::Point3D &p) const -> std::size_t {
     return std::hash<int>()(p.x) ^ std::hash<int>()(p.y << 16) ^
